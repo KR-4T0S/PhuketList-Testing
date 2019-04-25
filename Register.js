730938dbe5c2f833fe53a815a768b6a19509c362
Richie E <repinosa@gmail.com>
2019-04-24 18:34:16 -0700

    Add test environment for react app

---
+++ Register.js
@@ -0,0 +1,24 @@
+require('chromedriver');
+const assert = require('assert');
+const {Builder, Key, By, until} = require('selenium-webdriver');
+describe('Checkout http://localhost:3000/home', function () {
+    let driver;
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+    it('Register for phuketlist', async function() {
+        // Load the page
+        await driver.get('http://localhost:3000/home');
+        // Find the search box by id
+        await driver.findElement(By.linkText("Register")).click();
+        //search for text fields
+        await driver.findElement(By.id("name")).sendKeys("testUser");
+        await driver.findElement(By.id("email")).sendKeys("testUser@gmail.com");
+        await driver.findElement(By.id("password")).sendKeys("testUser");
+        await driver.findElement(By.className("btn btn-primary btn-block ")).click();
+        await driver.wait(until.elementLocated(By.className('sub-header')), 10000);
+        
+    });
+    // close the browser after running tests
+    after(() => driver && driver.quit());
+})
